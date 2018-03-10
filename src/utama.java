a81446f25932b1ca5f6cd9307606f3abd8a07649
Ahmad <Ahmad@matrix-polymer.com>
2018-03-10 09:52:00 +0700

    mencoba rengine, (gagal)

--- src/utama.java
+++ src/utama.java
@@ -1,47 +1,23 @@
-import javax.swing.*;
-import java.awt.*;
+import java.io.*;
+import org.rosuda.JRI.Rengine;
+import org.rosuda.JRI.REXP;
+import org.rosuda.JRI.RVector;
+import org.rosuda.JRI.RMainLoopCallbacks;
 
-public class utama extends javax.swing.JFrame{
+public class utama{
+	
+	REXP x;
+	String[] arg = new String[]{"--vanilla"};
+	Rengine re= new Rengine(arg, false, null);
 	
 	public utama() {
-		initComponent();
+		re.eval("x = 13.5");
+		System.out.println(re.eval("x").asDouble());
 	}
 	
-	private void initComponent() {
-		txtCari = new javax.swing.JTextField();
-		btnCari = new javax.swing.JButton();
-		jLabel1 = new javax.swing.JLabel();
-		
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1257, 720));
-        
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 14));
-        jLabel1.setText("Daftar itu.");
-        
-        JFrame myFrame = new JFrame("My Frame");
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER); textLabel.setPreferredSize(new Dimension(300, 100));
-        myFrame.getContentPane().add(textLabel, BorderLayout.CENTER);
-        myFrame.setLocationRelativeTo(null);
-        
-        myFrame.pack();
-        myFrame.setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				new utama().setVisible(true);
-			}
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(()->{
+			utama utm = new utama();
 		});
 	}
-public javax.swing.JTextField txtCari;
-public javax.swing.JButton btnCari;
-public javax.swing.JLabel jLabel1;
 }
